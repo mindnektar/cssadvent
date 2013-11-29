806eedbdddfbdd4a09df2f04ccd8b4b6ab47e6a5
Martin Denk <wyverns.spirit@gmail.com>
2013-11-29 22:48:24 +0100

    task 9

---
+++ tasks/9/logic.js
@@ -0,0 +1,38 @@
+var info = [
+    'Weihnachtszeit ist Süßigkeitenzeit',
+    'Was haben wir hier? Sechs unterschiedlich große Kästen mit Buchstaben am Rand? Höchst verdächtig. Sieht stark wie ein Puzzle aus, das in die richtige Reihenfolge gebracht werden möchte. Ich wette, hier wird gleich eine bahnbrechende Weisheit offenbart!',
+    '<strong>Aufgabe:</strong> Positioniere alle Kästen ganz oben rechts in die Ecke, so dass sie aufeinander liegen. Sorge jetzt dafür, dass sie so aufeinander liegen, dass ein sinnvoller Text lesbar wird!'
+];
+
+var hints = [
+    'Erinnerst du dich noch an die Aufgabe von vorgestern? Da hast du absolutes Positionieren gelernt. Jedes der Kästen ist bereits absolut positioniert und jeweils mit einem <span class="c">left</span>- und einem <span class="c">top</span>-Wert versehen. Welche Werte musst du ihnen geben, damit sie ganz oben links platziert werden? Übrigens: Du kannst entweder jedem der sechs Elemente mühselig eigene <span class="c">left</span>- und <span class="c">top</span>-Werte geben, oder aber du verwendest einen Selektor, der alle sechs Elemente zusammenfassen kann. Da können wir auch gleich überprüfen, ob du dich noch an die Regeln der Gewichtungen und den <span class="c">!important</span>-Zusatz von vor sechs Tagen erinnerst...',
+    'Du siehst, dass alle Kästen zwar aufeinander liegen, aber nicht in der Reihenfolge, die dich den Text lesen lassen würde. Um die Reihenfolge der Elemente aufeinander zu ändern, benutze die Eigenschaft <span class="c">z-index</span>. Dieser kann eine beliebige Zahl als Wert annehmen, wobei Folgendes gilt: Je höher der Wert, desto weiter vorne ist das Element.',
+    'Unsicher, in welche Reihenfolge die Elemente gehören? Orientiere dich an der Breite der Kästen. Der breiteste gehört nach ganz hinten, der schmalste nach vorne.'
+];
+
+function check() {
+    var msg;
+
+    $.each($('div'), function() {
+        if (parseInt($(this).css('left')) !== 0 || parseInt($(this).css('top')) !== 0) {
+            msg = 'Es sind noch nicht alle Kästen ganz oben links positioniert.';
+            return false;
+        }
+    });
+
+    if (msg) {
+        return msg;
+    }
+
+    if (
+        $('#green').css('z-index') > $('#blue').css('z-index') &&
+        $('#blue').css('z-index') > $('#orange').css('z-index') &&
+        $('#orange').css('z-index') > $('#purple').css('z-index') &&
+        $('#purple').css('z-index') > $('#red').css('z-index') &&
+        $('#red').css('z-index') > $('#brown').css('z-index')
+    ) {
+        return 'Jaaaa, Schokolaaade! Schokolade ist der Hammer! Mehr Schokolaaaaadeee...';
+    }
+
+    return 'Der Satz ist leider noch nicht lesbar.'
+}
---
+++ tasks/9/source.php
@@ -0,0 +1,6 @@
+<div id="red">ST G</div>
+<div id="blue">OK</div>
+<div id="brown">EIL</div>
+<div id="green">SCH</div>
+<div id="purple">DE I</div>
+<div id="orange">OLA</div>
